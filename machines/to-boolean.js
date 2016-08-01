08685174a69e34b08ed677942c5a18a84cde349a
Scott Gress <scottmgress@gmail.com>
2016-08-01 11:06:41 -0500

    Updated "to-boolean" machine to work solely with specific strings (+ added tests)

--- machines/to-boolean.js
+++ machines/to-boolean.js
@@ -1,13 +1,13 @@
 module.exports = {
 
 
-  friendlyName: 'Convert to boolean',
+  friendlyName: 'Convert string to boolean',
 
 
-  description: 'Convert the given input value to a boolean.',
+  description: 'Convert the given string value to a boolean.',
 
 
-  extendedDescription: 'If the value cannot be converted to a boolean, the `Not a boolean` exit will be triggered.',
+  extendedDescription: 'This machine specifically converts the strings \'true\' and \'1\' to the boolean `true`, and the strings \'false\', \'0\' and \'\' (the empty string) to the boolean `false`.  All other string values will trigger an error.  To simply determine whether a given string is "truthy", use the `Is truthy?` machine.',
 
 
   sideEffects: 'cacheable',
@@ -18,9 +18,10 @@ module.exports = {
 
   inputs: {
 
-    value: {
-      description: 'The value to convert to a boolean.',
-      example: '==='
+    string: {
+      description: 'The string to convert to a boolean.',
+      example: 'foo',
+      required: true
     }
 
   },
@@ -38,9 +39,16 @@ module.exports = {
 
 
   fn: function(inputs, exits) {
-
-    // Return the "truthiness" of the value through the `success` exit.
-    return exits.success(!!inputs.value);
+    // Return `true` through the `success` exit for "true" and "1".
+    if (inputs.string === 'true' || inputs.string === '1') {
+      return exits.success(true);
+    }
+    // Return `false` through the `success` exit for "false" and "0" and "".
+    if (inputs.string === 'false' || inputs.string === '0' || inputs.string === '') {
+      return exits.success(false);
+    }
+    // Otherwise thrown an error
+    return exits.error('The given value could not be converted to a boolean.');
 
   },
 
